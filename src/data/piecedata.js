768c32ae6a55fa92e5934dae4bd85d57d47077b1
gc <gac9@sfu.ca>
2023-02-18 13:39:34 -0800

    Created classes for pieces and added controller folder

---
+++ src/data/piecedata.js
@@ -0,0 +1,9 @@
+class PieceData {
+    constructor(name, x, y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+}
+
+export default PieceData;
---
+++ src/data/playerpiecedata.js
@@ -0,0 +1,13 @@
+import PieceData from "./piecedata";
+
+class PlayerPiece extends PieceData {
+    constructor(name, x, y) {
+        super(name, x, y);
+    }
+
+    move() {
+        
+    }
+}
+
+export default PlayerPiece;
