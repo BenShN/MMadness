245599cf12a10cca49c6c07451f91e54c9ebbdd5
gc <gac9@sfu.ca>
2023-02-18 19:47:47 -0800

    Created map with the ability to move

--- src/styles.js
+++ src/styles.js
@@ -1,18 +1,28 @@
 import styled from "styled-components";
 
 export const StyledMap = styled.div`
+border-style: content-box;
   display: flex;
   flex-wrap: wrap;
-  width: 1080px;
+  width: 100%;
   height: 100%;
-  /* background-image: ; */
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(https://cdn.discordapp.com/attachments/1076560020449263622/1076709526176276552/map.jpg);
 `;
 
 export const StyledMapPiece = styled.div`
+
+  box-sizing: border-box;
+
+  border-style: solid;
+  border-color: yellow;
+  border-width: 1px;
   display: inline-block;
   width: 12.5%;
   aspect-ratio: 1/1;
   button {
+    color: yellow;
     padding: 0;
     width: 100%;
     height: 100%;
